89e17ece01261ad2d4aaf286ed7c7f45ad946209
sheilak <sheila@Sheilas-MacBook-Air.local>
2015-06-06 12:54:32 +0100

    convert xml to json

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
 import sys
 import urllib
+import xml2json
 from xml.etree import ElementTree as ET
 
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'lib'))
 
 from flask import Flask, url_for, render_template
 app = Flask(__name__)
+
+class Options(object):
+    pretty = False
+
+def make_options(pretty):
+    options = Options()
+    options.pretty = pretty
+    return options
+
 
 
 @app.route('/')
@@ -16,11 +26,9 @@ def index(name=None):
 @app.route('/search/<searchTerm>')
 def api_shows(searchTerm):
     requestURL = 'http://services.tvrage.com/feeds/search.php?show=' + searchTerm 
-    return ET.tostring(ET.parse(urllib.urlopen(requestURL)).getroot())
+    root = ET.parse(urllib.urlopen(requestURL)).getroot()
 
-
-
-    
+    return xml2json.xml2json(ET.tostring(root),make_options(True))
 
 
 
