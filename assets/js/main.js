db4090924ab73f657f50b3feb8f8134345d13d5f
Allison Greene <allisongreene@Allisons-MacBook-Pro.local>
2015-06-06 16:06:00 +0100

    removing the hardcoded image urls styling - listing h2

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,5 +61,5 @@ watercooler.service('tvService', ['$q', '$http', function ($q, $http) {
         return deferred.promise;
     }
 
-    var tvRes = [{ id: '', name: 'Game of Thrones', image: 'http://images.tvrage.com/shows/3/2930.jpg', people: [{ name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, { name: 'Sheila', image: 'http://images.tvrage.com/shows/3/2930.jpg' }] }, { id: '', name: 'Orphan Black', image: 'http://images.tvrage.com/shows/3/2930.jpg', people: [{ name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, { name: 'Ali', image: 'http://images.tvrage.com/shows/3/2930.jpg' }] }]
+    var tvRes = [{ id: '', name: 'Game of Thrones', image: '', people: [{ name: 'Mary', image: '' }, { name: 'Sheila', image: '' }] }, { id: '', name: 'Orphan Black', image: '', people: [{ name: 'Mary', image: '' }, { name: 'Ali', image: '' }] }]
 }]);
