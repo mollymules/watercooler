25cf25658ff1336f4797014930b8bceceef2135e
mollymules <seerymary@hotmail.com>
2015-06-06 18:21:32 +0100

    link on shows to search

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,21 +2,22 @@ var watercooler = angular.module('watercooler', ['ngRoute']);
 
 watercooler.config(function ($routeProvider) {
     $routeProvider
-      .when('/', {
-          templateUrl: 'templates/index.html',
-          controller: 'mainCtrl'
-      })
-      .when('/', {
-          templateUrl: 'templates/search.html',
-          controller: 'mainCtrl'
-      })
-     .when('/all', {
-        templateUrl: 'templates/allShows.html',
-        controller: 'mainCtrl'
+     
+     .when('/', {
+         templateUrl: 'templates/shows.html',
+         controller: 'mainCtrl'
      })
+         .when('/search', {
+             templateUrl: 'templates/search.html',
+             controller: 'mainCtrl'
+         })
         .when('/add/:showID', {
             templateUrl: 'templates/add-people.html',
             controller: 'peopleCtrl'
+        })
+        .when('/episodes/:showID', {
+            templateUrl: 'templates/episode-list.html',
+            controller: 'episodeCtrl'
         })
       .otherwise({
           redirectTo: '/'
@@ -28,7 +29,6 @@ watercooler.controller('mainCtrl', ['$scope', '$q', 'tvService', function ($scop
     $scope.getAllShows = function () {
         tvService.getShows().then(function (result) {
             $scope.allShows = result;
-
         });
     }
 
@@ -40,11 +40,22 @@ watercooler.controller('mainCtrl', ['$scope', '$q', 'tvService', function ($scop
     }
 }]);
 
+watercooler.controller('episodeCtrl', ['$scope', '$q', '$routeParams', 'tvService', function ($scope, $q, $routeParams, tvService) {
+
+    $scope.showId = $routeParams.showID;
+
+    $scope.getEpisodes = function () {
+        tvService.getEpisodes().then(function (result) {
+            $scope.episodes = result;
+        });
+    }
+}]);
+
 watercooler.controller('peopleCtrl', ['$scope', '$routeParams', 'tvService', function ($scope, $routeParams, tvService) {
     $scope.showId = $routeParams.showID;
 
     tvService.getShow($scope.showId).then(function (result) {
-        $scope.showDetail = result;
+        $scope.showDetail = result.Show;
     });
 
     tvService.getPeople().then(function (result) {
@@ -53,13 +64,13 @@ watercooler.controller('peopleCtrl', ['$scope', '$routeParams', 'tvService', fun
 
     var selectedPeople = []
 
-    $scope.addPerson = function(person){
+    $scope.addPerson = function (person) {
         person.isAdded = true;
         selectedPeople.push(person.id);
         console.log($scope);
     }
 
-    $scope.removePerson = function(person){
+    $scope.removePerson = function (person) {
         person.isAdded = false;
         var index = selectedPeople.indexOf(person.id);
         if (index !== -1) {
@@ -67,78 +78,92 @@ watercooler.controller('peopleCtrl', ['$scope', '$routeParams', 'tvService', fun
         }
     }
 
-    $scope.addShow = function(){
+    $scope.addShow = function () {
         tvService.addShow($scope.showDetail, $scope.selectedPeople);
     }
 }])
 
 watercooler.service('tvService', ['$q', '$http', function ($q, $http) {
     this.getSchedule = function () {
-    
+
 
     }
-    
+
     this.getShow = function (showId) {
         var deferred = $q.defer();
-        setTimeout(function () {
-            var apiUrl = '//' + window.location.host + '/show/' + showId;
-            $http.get(apiUrl).success(function (data) {
-                deferred.resolve(data);
-            }).error(function () {
-                deferred.reject('None found');
-            });
-        }, 1000);
+        var apiUrl = '//' + window.location.host + '/show/' + showId;
+        $http.get(apiUrl).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('None found');
+        });
         return deferred.promise;
     }
 
     this.addShow = function (showDetail, people) {
         var deferred = $q.defer();
-        setTimeout(function () {
-            var apiUrl = '//' + window.location.host + '/show/' + showDetail.showid;
-            $http.post(apiUrl, {show:JSON.stringify(showDetail), people: JSON.stringify(people)}).success(function (data) {
-                deferred.resolve(data);
-            }).error(function () {
-                deferred.reject('None found');
-            });
-        }, 1000);
+
+        var apiUrl = '//' + window.location.host + '/show/' + showDetail.showid;
+        $http.post(apiUrl, { show: JSON.stringify(showDetail), people: JSON.stringify(people) }).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('None found');
+        });
+
         return deferred.promise;
     }
 
-    this.getPeople= function (showId) {
+    this.getPeople = function (showId) {
         var deferred = $q.defer();
         setTimeout(function () {
-           
-            deferred.resolve([{ id:'1',name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, {id:'2', name: 'Sheila', image: 'http://images.tvrage.com/shows/3/2930.jpg' }]);
-           
+
+            deferred.resolve([{ id: '1', name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, { id: '2', name: 'Sheila', image: 'http://images.tvrage.com/shows/3/2930.jpg' }]);
+
         }, 1000);
         return deferred.promise;
     }
 
     this.getShows = function () {
         var deferred = $q.defer();
-        setTimeout(function () {
-            var apiUrl = '//' + window.location.host + '/getShows';
-            $http.get(apiUrl).success(function (data) {
-                deferred.resolve(data);
-            }).error(function () {
-                deferred.reject('None found');
-            });
-        }, 1000);
+
+        var apiUrl = '//' + window.location.host + '/getShows';
+        $http.get(apiUrl).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('None found');
+        });
+
+        return deferred.promise;
+    }
+
+    this.getEpisodes = function (showId) {
+        var deferred = $q.defer();
+
+        var apiUrl = '//' + window.location.host + '/getEpisodes/' + showId;
+        $http.get(apiUrl).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('None found');
+        });
+
         return deferred.promise;
     }
 
     this.searchShows = function (searchString) {
         var deferred = $q.defer();
-        setTimeout(function () {
-            var apiUrl = '//' + window.location.host + '/search/' + searchString;
-            $http.get(apiUrl).success(function (data) {
-                deferred.resolve(data);
-            }).error(function () {
-                deferred.reject('None found');
-            });
-        }, 1000);
+
+        var apiUrl = '//' + window.location.host + '/search/' + searchString;
+        $http.get(apiUrl).success(function (data) {
+            deferred.resolve(data);
+        }).error(function () {
+            deferred.reject('None found');
+        });
+
         return deferred.promise;
     }
 
-    var tvRes = [{ id: '', name: 'Game of Thrones', image: '', people: [{ name: 'Mary', image: '' }, { name: 'Sheila', image: '' }] }, { id: '', name: 'Orphan Black', image: '', people: [{ name: 'Mary', image: '' }, { name: 'Ali', image: '' }] }]
+    var tvRes = [{
+        id: '', name: 'Game of Thrones', image: 'http://images.tvrage.com/shows/3/2930.jpg',
+        people: [{ name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, { name: 'Sheila', image: 'http://images.tvrage.com/shows/3/2930.jpg' }]
+    }, { id: '', name: 'Orphan Black', image: 'http://images.tvrage.com/shows/3/2930.jpg', people: [{ name: 'Mary', image: 'http://images.tvrage.com/shows/3/2930.jpg' }, { name: 'Ali', image: 'http://images.tvrage.com/shows/3/2930.jpg' }] }]
 }]);
